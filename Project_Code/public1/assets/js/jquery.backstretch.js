c87c7d70da6fa9b32d5046ae0660cad811b2ff39
kiranchaitanya22 <kiran.ilf9@gmail.com>
2017-05-29 11:15:31 -0700

    Update jquery.backstretch.js

--- Project_Code/public1/assets/js/jquery.backstretch.js
+++ Project_Code/public1/assets/js/jquery.backstretch.js
@@ -1,6 +1,6 @@
 /*! Backstretch - v2.0.4 - 2013-06-19
 * http://srobbin.com/jquery-plugins/backstretch/
-* Copyright (c) 2013 Scott Robbin; Licensed MIT */ 
+* Copyright (c) 2013 Scott Robbin; Licensed MIT */  
 
 ;(function ($, window, undefined) {
   'use strict';
